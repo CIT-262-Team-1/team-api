0a4328ffc07170cb883923a1990c1c771f523378
waheednazeer <waheed.nazeer@hotmail.com>
2024-02-20 20:25:48 +0500

    app.get('/waheed');

---
+++ server.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express();
+
+const port=3000;
+
+app.get('/',(req,res)=>{
+    res.send("Hell Team-1");
+});
+app.get('/sean',(req,res)=>{
+    res.send("Hello Sean");
+});
+
+app.listen(port,()=>{
+    console.log("Listening on port "+port);
+});
